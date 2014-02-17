3c0deb88ba76bb3281e60f8133b924599e4d2dde
EmnaZayani <emna.zayani@esprit.tn>
2014-02-17 18:08:06 +0100

    Affichage hello world

---
+++ src/test1/Test1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1;
+
+/**
+ *
+ * @author mac
+ */
+public class Test1 {
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Hello world");
+    }
+}
